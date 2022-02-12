2f388e5122887ea5e03a5e075369edc54d37b762
kakysh7 <durdyyevarnazar@gmail.com>
2022-02-12 08:25:41 +0500

    header elements update

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import './App.css';
 
 import HomePage from './components/pages/homepage/homepage.component';
 import ShopPage from './components/pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
       <Route exact path='/' component={HomePage}/>
       <Route exact path='/shop' component={ShopPage}/>
