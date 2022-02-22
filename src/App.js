8308d531002da3aabd24ce9737644fd15f4b5366
kakysh7 <durdyyevarnazar@gmail.com>
2022-02-22 17:37:27 +0500

    added firebas utils

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import HomePage from './components/pages/homepage/homepage.component';
 import ShopPage from './components/pages/shop/shop.component';
 import SignInAndSignUpPage from './components/pages/sign-in-and-up/sign-in-and-up.component';
 import Header from './components/header/header.component';
-import {auth} from './firebase/firebase.utils'
+import {auth, createUserProfileDocument} from './firebase/firebase.utils'
 
 class App extends React.Component{
   constructor() {
@@ -22,9 +22,23 @@ class App extends React.Component{
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user})
-    })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+          console.log(this.state);
+        });
+      }
+       
+        this.setState({currentUser: userAuth});
+    });
   }
 
   componentWillUnmount() {
--- src/components/pages/sign-in-and-up/sign-in-and-up.component.jsx
+++ src/components/pages/sign-in-and-up/sign-in-and-up.component.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
 import SignIn from "../../sign-in/sign-in.component";
+import SignUp from "../../sign-up/sign-up.component";
 
 import './sign-in-and-up.styles.scss';
 
 const SignInAndSignUpPage = () => (
-    <div className="sign-in-and-sign-up"><SignIn/></div>
+    <div className="sign-in-and-sign-up">
+        <SignIn/>
+        <SignUp/>
+        </div>
     
 )
 
